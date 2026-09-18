4a6526afa440c3b61135948fb1a0935a21341690
Kuldeep Wadhwa <kuldeep.wadhwa@justice.gov.uk>
2026-09-18 13:51:59 +0100

    updated path template

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   testDir: './e2e_tests/tests',
   expect: {
    toHaveScreenshot: {
-     pathTemplate: '{testDir}/{testFileDir}/{testName}/{arg}-{projectName}-{platform}{ext}',
+     pathTemplate: '{testDir}/{testFileDir}/{testFileName}/{arg}-{projectName}-darwin{ext}',
    },
  },
   /* Run tests in files in parallel */
