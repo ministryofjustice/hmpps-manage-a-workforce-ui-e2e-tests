1146ab3f33d4e7cde302fda47c598bf243cb282f
Kuldeep Wadhwa <kuldeep.wadhwa@justice.gov.uk>
2026-09-18 13:29:30 +0100

    commented browser run

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,10 +57,10 @@ export default defineConfig({
         ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     // {
     //   name: 'webkit',
